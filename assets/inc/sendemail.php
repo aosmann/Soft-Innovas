87cab13c1c81edf2b64ddfd7dce7e5795c7155e3
osman <aosmannur@gmail.com>
2022-08-04 23:35:47 +0300

    /'

--- assets/inc/sendemail.php
+++ assets/inc/sendemail.php
@@ -1,8 +1,8 @@
 <?php
 
 // Define some constants
-define( "RECIPIENT_NAME", "John Doe" );
-define( "RECIPIENT_EMAIL", "mail@mail.com" );
+define( "RECIPIENT_NAME", "Web Mail" );
+define( "RECIPIENT_EMAIL", "info@softinnovas.com" );
 
 // Read the form values
 $success = false;
